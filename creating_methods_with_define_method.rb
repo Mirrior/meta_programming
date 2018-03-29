6c2491690d88b3be0ec941d771d2314f9a4dad62
Rustin Player <rjplayer@outlook.com>
2018-03-29 11:52:09 -0600

    first commit

---
+++ creating_methods_with_define_method.rb
@@ -0,0 +1,14 @@
+class Author
+  genres = %w(fiction history coding textbook)
+
+  genres.each do |genre|
+    define_method("#{genre}_details") do |arg|
+      puts "Genre: #{genre}"
+      puts arg
+      puts genre.object_id
+    end
+  end
+end
+
+author = Author.new
+author.textbook_details "Hello Textbook"
---
+++ string_meta_programming.rb
@@ -0,0 +1,11 @@
+class String
+  def censor(bad_word)
+    self.gsub! "#{bad_word}", "CENSORED"
+  end
+
+  def number_of_chars
+    size
+  end
+end
+
+p "The bunny was in trouble with the king's bunny".number_of_chars
